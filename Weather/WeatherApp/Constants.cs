a9350433c29d52f67caca7679210781ea0314fa9
michalmelis <michalmelis.sk@gmail.com>
2020-03-01 22:33:47 +0100

    Add project files.

---
+++ Weather/WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "2b8a310c0d7331070e5c62e58451aa36";
+    }
+}
---
+++ Weather/WeatherApp/FahrenheitToCelsiusConvertet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    class FahrenheitToCelsiusConvertet : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Math.Truncate(((double)value - 32) * 5 / 9).ToString() + "\x00B0" + "C";
+        }    
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }     
+    }
+}
